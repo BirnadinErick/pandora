95e9cd65b037bfb93702163e23b08b68ee7c33bd
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2023-07-27 17:01:35 +0530

    feat: add `discover_files()`
    `discover_files()` searches the given *root* for any .md files and returns Vec<String>, each entry for file-path relative to the given *root*

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use glob::glob;
 use regex::{Captures, Regex};
 use std::collections::HashMap;
 use std::fs::File;
@@ -19,6 +20,18 @@ pub fn read_file(name: &str) -> String {
     content
 }
 
+fn discover_files(root: &str) -> Vec<String> {
+    let mut files: Vec<String> = Vec::new();
+    for p in glob(&format!("{}/**/*.md", root))
+        .unwrap()
+        .filter_map(std::result::Result::ok)
+    {
+        files.push(p.display().to_string())
+    }
+
+    files
+}
+
 fn parse_axons(md: &str, _map: Option<()>, host: &str) -> String {
     let regex =
         Regex::new(r"(?mU)\[{2}(?P<id>[a-zA-Z\s\-\d]+)\|{1}(?P<desc>[a-zA-Z\s\-\d_]*)\]{2}")
@@ -26,23 +39,40 @@ fn parse_axons(md: &str, _map: Option<()>, host: &str) -> String {
 
     let substitution = format!("[$desc]({}/$id.html)", host);
 
-    regex
-        .replace_all(md, |caps: &Captures| {
-            let id = &caps["id"];
-            println!("{}", id);
-            substitution.clone()
-        })
-        .into()
+    // regex
+    //     .replace_all(md, |caps: &Captures| {
+    //         let id = &caps["id"];
+    //         println!("{}", id);
+    //         substitution.clone()
+    //     })
+    //     .into()
+    // let res: String = regex.replace_all(md, substitution).into();
+    // println!("{}", res);
+    // res
+
+    regex.replace_all(md, substitution).into()
 }
 
 fn main() {
     let host = "https://eb.birn.cc";
-    let content = read_file("input.md");
+    let root = "eb";
+    let files = discover_files(root);
+    let content: Vec<(String, String)> = files
+        .into_iter()
+        .map(|f| (f.clone(), read_file(f.as_str().clone())))
+        .collect();
+    // let content = read_file("input.md");
 
-    let mut dir: HashMap<&str, &str> = HashMap::new();
-    dir.insert("input.md", &content);
+    let dir: HashMap<String, String> =
+        content.into_iter().fold(HashMap::new(), |mut acc, (f, c)| {
+            acc.insert(f, c);
+            acc
+        });
+    // dir.insert("input.md", &content);
 
-    for (_, v) in dir.iter() {
-        println!("{}", parse_axons(*v, None, host));
+    for (f, v) in dir.iter() {
+        println!("{}:", f);
+        println!("{}", parse_axons(&v, None, host));
+        println!("");
     }
 }
